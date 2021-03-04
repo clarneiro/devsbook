93c3c9c3dda885f64c1bef6175343d3b14cb3dc3
clarneiro <62855166+clarneiro@users.noreply.github.com>
2021-03-04 10:51:45 -0300

    Add files via upload

---
+++ LoginController.php
@@ -0,0 +1,90 @@
+<?php
+namespace src\controllers;
+
+use \core\Controller;
+use \src\handlers\LoginHandler;
+
+class LoginController extends Controller {
+
+    public function signin(){
+        $flash ='';
+        if(!empty($_SESSION['flash'])){
+            $flash=$_SESSION['flash'];
+            $_SESSION['flash']='';
+        }
+        $this->render('signin',['flash'=>$flash]);
+    } 
+
+    public function signinAction(){
+        $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+        $password=filter_input(INPUT_POST, 'password');
+
+        if($email && $password){
+
+            $token=LoginHandler::verifyLogin($email, $password);
+            if($token){
+                $_SESSION ['token']=$token;
+                $this->redirect('/');
+            } else{
+                $_SESSION['flash']='email e/ou senha incorretos';
+                $this->redirect('/login');
+            }
+
+        } else{
+            $_SESSION['flash']='Digite os campos de email e/ou senha';
+            $this->redirect('/login');
+        }
+    }
+
+    public function signup(){
+        $flash ='';
+        if(!empty($_SESSION['flash'])){
+            $flash=$_SESSION['flash'];
+            $_SESSION['flash']='';
+        }
+        $this->render('signup',['flash'=>$flash]);
+    }
+
+    public function signupAction(){
+
+        $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+        $password = filter_input(INPUT_POST, 'password');
+        $name = filter_input(INPUT_POST, 'name');
+        $birthdate = filter_input(INPUT_POST, 'birthdate');
+
+
+
+        if($email && $password && $name && $birthdate){
+
+            $birthdate = explode('/', $birthdate);
+
+            if(count($birthdate)==3){
+                
+
+                $birthdate=$birthdate[2].'-'.$birthdate[1].'-'.$birthdate[0];
+                if(strtotime($birthdate) === false){
+                    $_SESSION['flash']='Data de nascimento inválida';
+                    $this->redirect('/cadastro');
+                }
+
+                if(LoginHandler::emailExists($email)=== false){
+
+
+                    $token = LoginHandler::addUser($name, $email, $password, $birthdate);
+                    $_SESSION['token']= $token;
+                    $this->redirect('/');
+                } else{
+                    $_SESSION['flash']= ' email já cadastrado.';
+                    $this->redirect('/cadastro');
+                }
+
+            } else{
+               $_SESSION['flash']='Data de nascimento inválida';
+                $this->redirect('/cadastro');
+            }
+        }
+    }
+
+
+
+}
